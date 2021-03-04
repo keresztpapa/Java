e807035f492367a7955f8d7ea133cad35cfe4cb6
keresztpapa <48327504+keresztpapa@users.noreply.github.com>
2021-03-04 17:18:11 +0100

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+
+        JFrame frame = new JFrame();
+        frame.setSize(960,680);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+
+        frame.setVisible(true);
+    }
+
+}
